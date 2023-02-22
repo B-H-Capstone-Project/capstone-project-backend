f1cb8fa6f43e001a2fc8d42dcea513f5823fa3e9
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2023-02-22 11:40:29 -0700

    Fixes error message and function name properly

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,7 +1,7 @@
 import { Request, RequestHandler, Response } from 'express';
-import * as reservationService /* { createReservation, deleteReservation, getReservationById, updateReservation, getReservationByUser } */ from '../services/reservation.service';
+import * as reservationService from '../services/reservation.service';
 
-export const createReservations: RequestHandler = async (req: Request, res: Response) => {
+export const createReservation: RequestHandler = async (req: Request, res: Response) => {
   let addressId = req.body.address_id;
   let userId = req.body.user_id;
 
@@ -13,14 +13,17 @@ export const createReservations: RequestHandler = async (req: Request, res: Resp
       reservation,
     });
   } catch (error) {
-    console.error('[teams.controller][getTeams][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+    console.error(
+      '[reservation.controller][createReservations][Error] ',
+      typeof error === 'object' ? JSON.stringify(error) : error
+    );
     res.status(500).json({
-      message: 'There was an error when fetching teams',
+      message: 'There was an error when creating Reservation',
     });
   }
 };
 
-export const deleteReservations: RequestHandler = async (req: Request, res: Response) => {
+export const deleteReservation: RequestHandler = async (req: Request, res: Response) => {
   try {
     const reservationId = req.params.id;
     const reservation = await reservationService.deleteReservation(reservationId);
@@ -28,9 +31,12 @@ export const deleteReservations: RequestHandler = async (req: Request, res: Resp
       reservation,
     });
   } catch (error) {
-    console.error('[teams.controller][getTeams][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+    console.error(
+      '[reservation.controller][deleteReservation][Error] ',
+      typeof error === 'object' ? JSON.stringify(error) : error
+    );
     res.status(500).json({
-      message: 'There was an error when fetching teams',
+      message: 'There was an error when deleting reservation',
     });
   }
 };
@@ -43,14 +49,17 @@ export const getReservationsById: RequestHandler = async (req: Request, res: Res
       reservation,
     });
   } catch (error) {
-    console.error('[teams.controller][getTeams][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+    console.error(
+      '[reservation.controller][getReservationsById][Error] ',
+      typeof error === 'object' ? JSON.stringify(error) : error
+    );
     res.status(500).json({
-      message: 'There was an error when fetching teams',
+      message: 'There was an error when get reservations by id',
     });
   }
 };
 
-export const updateReservations: RequestHandler = async (req: Request, res: Response) => {
+export const updateReservation: RequestHandler = async (req: Request, res: Response) => {
   try {
     const reservation_id = req.params.id;
 
@@ -62,14 +71,17 @@ export const updateReservations: RequestHandler = async (req: Request, res: Resp
       reservation,
     });
   } catch (error) {
-    console.error('[teams.controller][getTeams][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+    console.error(
+      '[reservation.controller][updateReservation][Error] ',
+      typeof error === 'object' ? JSON.stringify(error) : error
+    );
     res.status(500).json({
-      message: 'There was an error when fetching teams',
+      message: 'There was an error when updatig reservation',
     });
   }
 };
 
-export const getReservationsByUsers: RequestHandler = async (req: Request, res: Response) => {
+export const getReservationsByUser: RequestHandler = async (req: Request, res: Response) => {
   try {
     const userId = req.params.user_id;
     const reservation = await reservationService.getReservationByUser(userId);
@@ -77,23 +89,23 @@ export const getReservationsByUsers: RequestHandler = async (req: Request, res: 
       reservation,
     });
   } catch (error) {
-    console.error('[teams.controller][getTeams][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+    console.error(
+      '[reservation.controller][getReservationsByUsers][Error] ',
+      typeof error === 'object' ? JSON.stringify(error) : error
+    );
     res.status(500).json({
-      message: 'There was an error when fetching teams',
+      message: 'There was an error when get reservations by user',
     });
   }
 };
 
 export const getReservations: RequestHandler = async (req: Request, res: Response) => {
   try {
-    console.log('inside get reservations');
     const reservations = await reservationService.getReservations();
 
     res.status(200).json({
       reservations,
     });
-
-    console.log('can get reservations');
   } catch (error) {
     console.error(
       '[reservation.controller][getReservations][Error] ',
--- src/router/reservation.router.ts
+++ src/router/reservation.router.ts
@@ -5,19 +5,19 @@ const router = express.Router();
 
 // Reservation
 // Create reservation
-router.post('/reservation', reservationController.createReservations);
+router.post('/reservation', reservationController.createReservation);
 
 // Get Reservation By User Id
-router.get('/reservation/user/:user_id', reservationController.getReservationsByUsers);
+router.get('/reservation/user/:user_id', reservationController.getReservationsByUser);
 
 // Get Reservation By Resrvation Id
 router.get('/reservation/:id', reservationController.getReservationsById);
 
 // Update Reservation by Id
-router.put('/reservation/:id', reservationController.updateReservations);
+router.put('/reservation/:id', reservationController.updateReservation);
 
 // Delete Reservation by Id
-router.delete('/reservation/:id', reservationController.deleteReservations);
+router.delete('/reservation/:id', reservationController.deleteReservation);
 
 // Get All Reservation (admin side)
 router.get('/reservations', reservationController.getReservations);
