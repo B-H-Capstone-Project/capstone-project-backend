80919ee22763bc076bec00918ec167aa06ea7265
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2023-03-08 11:24:16 -0700

    change get reservation user queries

--- src/models/reservationUser.queries.ts
+++ src/models/reservationUser.queries.ts
@@ -1,4 +1,4 @@
 export const ReservationUserQueries = {
   GetReservationsUsers:
-    'SELECT reservation.user_id "user_id", first_name, last_name, phone_number, address_line1, address_line2,  city,  province,  postal_code,  country,  profile,  reservation.id "reservation_id",  type,  date,  description  FROM reservation LEFT OUTER JOIN user ON user.id = reservation.user_id  where user.is_active=1 and is_confirmed=1;',
+    'SELECT reservation.user_id "user_id", first_name, last_name, phone_number, address_line1, address_line2,  city,  province,  postal_code,  country,  profile,  reservation.id "reservation_id",  type,  date,  description  FROM reservation LEFT OUTER JOIN user ON user.id = reservation.user_id  where user.is_active=1;',
 };
